12b9a80bb1eb341da8029ec8fd2acbcd48ad34a4
AdriGF <agomezf24@fpcoruna.afundacion.org>
2025-11-20 11:00:12 +0100

    Segundo endpoint hecho

--- sprint3django/RestAPI/webserviceapp/views.py
+++ sprint3django/RestAPI/webserviceapp/views.py
@@ -1,3 +1,4 @@
+
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 from .models import Libros
@@ -17,3 +18,20 @@ def devolver_libros(request):
 		diccionario['año_publicacion'] = fila_sql.año_publicacion
 		respuesta_final.append(diccionario)
 	return JsonResponse (respuesta_final, safe=False)
+
+def devolver_libros_por_id(request, id_solicitado):
+        libro = Libros.objects.get(id = id_solicitado)
+        comentarios = libro.comentarios_set.all()
+        lista_comentarios = []
+        for fila_comentario_sql in comentarios:
+                diccionario = {}
+                diccionario['ID'] = fila_comentario_sql.id
+                diccionario['Comentario'] = fila_comentario_sql.comentario
+                lista_comentarios.append(diccionario)
+        resultado = {
+                'id': libro.id,
+                'nombre': libro.nombre,
+                'año_publicacion': libro.año_publicacion,
+                'comentarios': lista_comentarios
+        }
+        return JsonResponse(resultado, json_dumps_params={'ensure_ascii': False})
